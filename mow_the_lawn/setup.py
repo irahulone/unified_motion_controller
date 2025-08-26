f4ad0de567a0ddeefa09fa4425770164a668eaa2
galaxyw222 <wtierney@scu.edu>
2025-08-26 16:15:16 -0700

    Add files via upload

--- mow_the_lawn/setup.py
+++ mow_the_lawn/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'unified_controller'
+package_name = 'mow_the_lawn'
 
 setup(
     name=package_name,
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'unified_controller = unified_controller.unified_controller:main',
+            'mow_the_lawn = mow_the_lawn.mow_the_lawn:main',
         ],
     },
 )
